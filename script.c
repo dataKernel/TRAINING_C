c1bfae286e0797e7bad8afd8276eea84a860a1df
data_kernel <lancelot@valoo.home>
2022-08-31 17:27:02 +0100

    adding script file test

--- script.c
+++ script.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char    *ft_strupcase(char *str)
-{
-    int     index;
-
-    index = 0;
-    while(str[index])
-    {
-        str[index] -= 32;
-        index++;
-    }
-    return(str);
-}
-
 int     main(void)
 {
-    char    str[100] = "lancelot";
-
-    printf("pre func:: %s\n", str);
-    ft_strupcase(str);
-    printf("post func:: %s\n", str);
     return(0);
 }
