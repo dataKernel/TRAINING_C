3ad9862aa953759f7917bb091d665bb365aea09c
data_kernel <lancelot@valoo.home>
2022-08-31 18:08:18 +0100

    update lowercase

--- script.c
+++ script.c
@@ -8,12 +8,18 @@ char    *ft_strlowcase(char *str)
     index = 0;
     while(str[index])
     {
-        str[index] += 
+        if(str[index] >= 'A' && str[index] <= 'Z')
+            str[index] += 32;
         index++;
     }
+    return(str);
 }
 
 int     main(void)
 {
+    char    str[100] = "LANCeLOT";
+    printf("pre func:: %s\n", str);
+    ft_strlowcase(str);
+    printf("post func:: %s\n", str);
     return(0);
 }
