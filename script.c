7c63038f3d2603ed800d7c504919c946a8dd69b1
data_kernel <lancelot@valoo.home>
2022-08-31 18:11:47 +0100

    reset script

--- script.c
+++ script.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-char    *ft_strlowcase(char *str)
-{
-    int     index;
-
-    index = 0;
-    while(str[index])
-    {
-        if(str[index] >= 'A' && str[index] <= 'Z')
-            str[index] += 32;
-        index++;
-    }
-    return(str);
-}
-
 int     main(void)
 {
-    char    str[100] = "LANCeLOT";
-    printf("pre func:: %s\n", str);
-    ft_strlowcase(str);
-    printf("post func:: %s\n", str);
     return(0);
 }
