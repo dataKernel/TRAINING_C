1e53db6db66a35a40417c60f26b9445b5a48b0d3
data_kernel <lancelot.saintleger@gmail.com>
2022-09-04 13:46:02 +0100

    adding strcat

---
+++ ft_strcat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while(str[i])
+        i++;
+    return(i);
+}
+
+char    *ft_strcat(char *dst, char *src)
+{
+    int     sizeDst;
+    int     i;
+
+    sizeDst = ft_strlen(dst);
+    i = 0;
+    while(src[i])
+    {
+        *(dst + (i + sizeDst)) = *(src + i);
+        i++;
+    }
+    *(dst + (i + 1 + sizeDst)) = '\0';
+    return(dst);
+}
+
+int     main(void)
+{
+    char    dst[100] = "lancelot";
+
+    printf("%s\n", ft_strcat(dst, "abc"));
+    return(0);
+}
